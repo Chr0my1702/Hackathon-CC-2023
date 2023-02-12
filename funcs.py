f1a58738b008cddfbfda4df30cd400e0960efc43
Chr0my1702 <49107678+Chr0my1702@users.noreply.github.com>
2023-02-12 16:42:59 +0000

    some logic

--- funcs.py
+++ funcs.py
@@ -1,6 +1,6 @@
 import pyttsx3
 from gtts import gTTS
-
+import json
 
 def auto_format(text, max_length=45, separator=' '):
     words = text.split(separator)
@@ -14,10 +14,44 @@ def auto_format(text, max_length=45, separator=' '):
         line_length += len(word) + 1
     return new_text
 
+def unformat(text):
+    return text.replace('\n', ' ')
+
 
 def speak_text(text):
-
     engine = pyttsx3.init()
-    engine.say(text)
+    engine.say(unformat(text))
     engine.setProperty('voice', 2)
     engine.runAndWait()
+
+
+def update_objective(objective_text, new_objective):
+    objective_text.text = "Objective: " + new_objective
+
+
+def update_money(money_text, new_money):
+    money_text.text = "Money: " + str(new_money)
+
+
+def update_task(objective_text, money_text, new_task):
+    update_objective(objective_text, new_task.objective)
+    update_money(money_text, new_task.money)
+
+
+def get_quest(level):
+    with open("npc_quests.json", "r") as file:
+        data = json.load(file)
+
+    # Return the quest information for the specified level
+    if level > 0 and level <= len(data):
+        quest = data[level - 1]
+        return quest["npc_task"], quest["reward"], quest["chemical"], quest["hint"]
+    else:
+        return None
+
+
+def set_quest(money_text, objective_text, level, NPC_Talk):
+    task, reward, chemical, hint = get_quest(level)
+    update_objective(objective_text, "Get " + chemical)
+    update_money(money_text, reward)
+    NPC_Talk.text = auto_format(task)
--- main.py
+++ main.py
@@ -85,6 +85,17 @@ class Sky(Entity):
             double_sided=True)
 
 
+class Holding_Object:
+    def __init__(self):
+        self.current_object = None
+
+    def set_object(self, new_object):
+        self.current_object = new_object
+
+    def get_object(self):
+        return self.current_object
+
+
 class Hand(Entity):
     def __init__(self):
         super().__init__(
@@ -101,7 +112,11 @@ class Hand(Entity):
         self.position = Vec2(0.4, -0.6)
 
 
+in_task = False
+
+
 def npc_action():
+    global in_task
     NPC_Talk.enabled = False
     text_size = NPC_Talk.world_scale
     button1_pos = ((NPC_Talk.position.x - text_size.x / 2 + .25)
@@ -113,7 +128,16 @@ def npc_action():
     agree.scale = (.3, .1)
     agree.position = button1_pos
     agree.text_entity.size = 1
-    agree.on_click = Func(setattr, NPC_Talk, 'enabled', False)
+
+    def set_in_task():
+        global in_task
+        in_task = not in_task
+
+    def disable_talk():
+        setattr(NPC_Talk, 'enabled', False)
+        set_in_task()
+
+    agree.on_click = Func(disable_talk)
 
     disagree.text = "No"
     disagree.scale = (.3, .1)
@@ -122,15 +146,37 @@ def npc_action():
     disagree.on_click = Func(setattr, NPC_Talk, 'enabled', False)
 
     if player.position.x >= npc.position.x - 4 and player.position.x <= npc.position.x + 4 and player.position.z >= npc.position.z - 4 and player.position.z <= npc.position.z + 4:
-        NPC_Talk.enabled = True
-        agree.enabled = True
-        disagree.enabled = True
+        print(in_task)
+        print(holding.get_object())
+        if in_task == False:
+            NPC_Talk.enabled = True
+            agree.enabled = True
+            disagree.enabled = True
 
-        NPC_Talk.text = auto_format(
-            "Hello, I am a NPC. I am here to help you. I will give you a quest. Do you want to accept it?")
-        thread = threading.Thread(target=speak_text, args=(NPC_Talk.text,))
-        thread.start()
-        NPC_Talk.background = True
+            thread = threading.Thread(target=speak_text, args=(NPC_Talk.text,))
+            thread.start()
+            NPC_Talk.background = True
+        elif in_task == True and holding.get_object() != "potassium hydrochloride":
+            NPC_Talk.text = "hmm, doesnt look like you have the thing i need in your hand right now, come back when you have it!"
+            NPC_Talk.enabled = True
+
+            invoke(setattr, NPC_Talk, 'enabled', False, delay=5)
+        elif in_task == True and holding.get_object() == "potassium hydrochloride":
+            NPC_Talk.text = "Oh, you have the thing i need! Thank you so much! Here is your money!"
+            NPC_Talk.enabled = True
+            invoke(setattr, NPC_Talk, 'enabled', False, delay=5)
+
+        else:
+            pass
+
+
+# Define the objective text and money text
+hint_popup = Text(text="Press H for a hint with the quest!", position=(
+	-0.3, .45), origin=(0, 0), color=color.white, scale=1, background=True)
+objective_text = Text(text="Objective: Get Potassium hydrochloride", position=(
+	0.3, .45), origin=(0, 0), color=color.white, scale=1, background=True)
+money_text = Text(text="Money: 0₫", position=(0.7, .45), origin=(
+	0, 0), color=color.yellow, scale=1,  background=True)
 
 
 npc_model = load_model("npc.glb")
@@ -142,13 +188,23 @@ npc = Entity(
     collider='box',
     on_click=npc_action)
 
+
 for z in range(30):
     for x in range(30):
         voxel = Voxel(position=(x, 0, z))
 
+
+def inisialise():
+	set_quest(money_text, objective_text, 2, NPC_Talk)
+	holding.set_object("none")
+
+
 player = FirstPersonController()
 player.set_position((10, 0, 10))
+
 sky = Sky()
 hand = Hand()
+holding = Holding_Object()
 
+inisialise()
 app.run()
